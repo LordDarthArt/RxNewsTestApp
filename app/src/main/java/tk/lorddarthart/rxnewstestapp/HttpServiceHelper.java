b73cc53dbbbd0d918af50884242cdf69aa31c317
LordDarthArt <lorddarthart@gmail.com>
2019-01-20 17:38:53 +0300

    -

--- app/src/main/java/tk/lorddarthart/rxnewstestapp/HttpServiceHelper.java
+++ app/src/main/java/tk/lorddarthart/rxnewstestapp/HttpServiceHelper.java
@@ -5,7 +5,7 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class HttpServiceHelper {
+class HttpServiceHelper {
     private static HttpServiceHelper mInstance;
     private static final String BASE_URL = "https://drive.google.com/uc?id=1wozWr5swgtdV9PLyo2b09mtjaOD6sS2I";
     private Retrofit mRetrofit;
@@ -16,21 +16,21 @@ public class HttpServiceHelper {
 
         OkHttpClient.Builder client = new OkHttpClient.Builder()
                 .addInterceptor(interceptor);
-        
+
         mRetrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
 
-    public static HttpServiceHelper getInstance() {
+    static HttpServiceHelper getInstance() {
         if (mInstance == null) {
             mInstance = new HttpServiceHelper();
         }
         return mInstance;
     }
 
-    public JSONPlaceHolderApi getJSONApi() {
+    JSONPlaceHolderApi getJSONApi() {
         return mRetrofit.create(JSONPlaceHolderApi.class);
     }
 }
